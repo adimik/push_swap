78fe2b785d4236809fa395c65046cb03c0ace64b
Dimitre Dimitrov <didimitr@c1r10s2.42luxembourg.lu>
2025-03-01 21:14:55 +0100

    01

---
+++ push_swap.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: didimitr <didimitr@student.42luxembourg    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/01 19:12:29 by didimitr          #+#    #+#             */
+/*   Updated: 2025/03/01 19:45:30 by didimitr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ll_get(int argc, char **argv)
+{
+	t_stack_a	list;
+	list = get_data();
+
+
+
+}
+int	main(int argc, char **argv)
+{
+	
+	ll_gen(argc, argv);
+
+
+
+	return(0);
+}
---
+++ push_swap.h
@@ -0,0 +1,13 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include <stdlib.h>
+
+typedef struct s_stack
+{
+    int num;
+    struct s_stack *next;
+}t_stack;
+
+
+#endif
